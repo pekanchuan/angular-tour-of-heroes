ca21fab61b411c2f87d97c70d597a48c0ca264d3
Chia Pha <dozeuto@gmail.com>
2022-01-16 18:57:44 +0800

    add service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,22 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
-
-import { FormsModule } from '@angular/forms';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
+import { MessagesComponent } from './messages/messages.component';
 
 @NgModule({
-  declarations: [AppComponent, HeroesComponent, HeroDetailComponent],
-  imports: [BrowserModule, AppRoutingModule, FormsModule],
-  providers: [],
+  declarations: [
+    AppComponent,
+    HeroesComponent,
+    HeroDetailComponent,
+    MessagesComponent,
+  ],
+  imports: [BrowserModule, FormsModule],
+  providers: [
+    // no need to place any providers due to the `providedIn` flag...
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
