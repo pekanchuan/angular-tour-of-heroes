a324ca8f9040f165c458b8ac48ca219dea42e11a
CHia <dozeuto@gmail.com>
2022-05-04 23:26:34 +0800

    Display a List

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+
 import { Hero } from '../hero';
+import { HEROES } from '../mock-heroes';
 
 @Component({
   selector: 'app-heroes',
@@ -12,7 +14,15 @@ export class HeroesComponent implements OnInit {
     name: 'Windstorm',
   };
 
+  selectedHero?: Hero;
+
+  heroes = HEROES;
+
   constructor() {}
 
   ngOnInit(): void {}
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
 }
